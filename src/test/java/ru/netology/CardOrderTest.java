d7de7a2ae9311cf2b2b8951d959e60833f5df83c
Nadya Chistyakova <npodtynchenko@list.ru>
2020-12-05 21:30:41 +0300

    edit

---
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -0,0 +1,27 @@
+package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:7777");
+    }
+
+    @Test
+    public void shouldSubmitRequest() {
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79210000000");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
